bac6e59354eccb996f03fb998e500faf449d5977
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-02-07 07:58:12 -0800

    init

---
+++ LINQ Whiteboard Problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Whiteboard_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
